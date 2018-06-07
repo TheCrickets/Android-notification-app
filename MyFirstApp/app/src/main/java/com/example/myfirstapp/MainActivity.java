f386dfd54323d24199f6ecc2ee9f8f5b06cdd7dd
Unknown <das.andreea@gmail.com>
2018-06-07 15:35:06 +0300

    Front-end of android app
    Implemented the front-end of the android app for our project. The user can choose what kind of notifications he/she would like to receive if he/she wishes to receive notifications.

---
+++ MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.myfirstapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    /** Called when the user taps the Send button
+    public fun sendMessage(view : View)
+    {
+        // Do something in response to button
+    }
+     */
+}
+
+
+//Un buton on/off daca vrea notificări
+//Checkboxes cu ce dezastre vrea sa primeasca,
+// deocamdată doar 1, dar fa sa mai pui adăuga ușor și altele
